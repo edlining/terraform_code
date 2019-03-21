006818ad4189568dfe8bf4a2218aa6044dbbe6c1
edoardo linington <edlining@edoardos-iMac.home>
2019-03-21 12:04:09 +0000

    added remote state tf

--- example.tf
+++ example.tf
@@ -5,11 +5,10 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
-  instance_type = "var.instance_type"
-
+  instance_type = "${var.instance_type}"
 # Tells Terraform that this EC2 instance must be created only after the
 # S3 bucket has been created.
-  depends_on = ["aws_s3_bucket.example"]
+  depends_on = ["aws_s3_bucket.RStf"]
 
 provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
@@ -21,14 +20,19 @@ resource "aws_eip" "ip" {
 }
 
 # New resource for the S3 bucket our application will use.
-resource "aws_s3_bucket" "example" {
-  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
-  # this name must be changed before applying this example to avoid naming
-  # conflicts.
-  bucket = "terraform-getting-started-guide"
+resource "aws_s3_bucket" "RStf" {
+  bucket = "remotestatetf"
   acl    = "private"
 }
 
+# remote state S3
+terraform {
+  backend "s3" {
+    bucket = "remotestatetf"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 output "ip" {
   value = "${aws_eip.ip.public_ip}"
 }
